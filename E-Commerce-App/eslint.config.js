df4361dda735f5dd56b4ac2fbcc89d0be25903ba
aniket-haptiq <aniket.kumare@haptiq.com>
2025-08-08 12:09:40 +0530

    ok

--- E-Commerce-App/eslint.config.js
+++ E-Commerce-App/eslint.config.js
@@ -2,11 +2,9 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { globalIgnores } from 'eslint/config'
-import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default ([
+    { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     extends: [
